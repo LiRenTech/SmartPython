2820e17f7ae8ae832703f9bc68b5a71644d96eae
littlefean <2028140990@qq.com>
2022-10-18 21:02:44 +0800

    内置对象分类

---
+++ 视频还没录但计划的/050 内置object大总结/分类.py
@@ -0,0 +1,31 @@
+allList = [
+    # 基础
+    print, help, input, eval, exec,
+    # 数学
+    abs, round, divmod, sum, max, min, pow,
+    hex, oct, bin,
+    # 函数式
+    map, filter,
+    # 字符
+    ascii, str, repr, ord, chr, bytearray, format,
+    # 基础数据类型
+    int, open, bool, bytes, complex, float,
+    # 面向对象
+    object,
+    staticmethod, classmethod,
+    isinstance, type, issubclass, super,
+    setattr, getattr, delattr, hasattr,
+    dir, id, property,
+    slice, callable, hash,
+    # 算法相关
+    len, range, reversed, enumerate, sorted, any, all, zip,
+    # 迭代
+    iter, next,
+    # 基础组合数据类型
+    tuple, list, set, dict, frozenset,
+    # 代码相关
+    vars, compile, globals, locals, memoryview,
+    # 其他
+    __import__,
+    quit,
+]
