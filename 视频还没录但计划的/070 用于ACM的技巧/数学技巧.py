32766f8a1bda911964a49b7513c9584fa5cf3454
littlefean <2028140990@qq.com>
2022-10-20 15:27:12 +0800

    更新目录

---
+++ 视频还没录但计划的/070 用于ACM的技巧/数学技巧.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+PyCharm 数学技巧
+2022年10月19日
+by littlefean
+"""
+from typing import *
+from math import *
+
+
+def main():
+    print(comb(5, 2))
+    return None
+
+
+if __name__ == "__main__":
+    main()
